a231a28e510dd0f092f950fcdfed8437abb4d575
Gift111 <giftchukwudi99@gmail.com>
2022-09-08 15:57:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ *
+ * Return: Always 0 (it is a success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle")
+		return (0);
+}
