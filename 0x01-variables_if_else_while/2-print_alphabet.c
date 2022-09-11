2359571694cafc992873dbb3c3984cba40b368a5
Gift111 <giftchukwudi99@gmail.com>
2022-09-11 12:58:12 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry lowercase alphabets
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	char lowch;
+
+	for (lowch = 'a'; low <= 'z'; c++)
+		putchar(lowch);
+		printf("\n");
+	return (0);
+
+}
