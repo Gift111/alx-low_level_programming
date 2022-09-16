5fa964d0b19ebe2cb35749f8a212d530f59287fe
Gift111 <giftchukwudi99@gmail.com>
2022-09-15 22:19:43 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,33 +10,30 @@
 
 int main(void)
 {
-	int num = 1;
+	int i;
 
-	while (num++ < 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz");
 		}
-		else if ((num % 3) == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Fizz ");
+			printf("Buzz");
 		}
-		else if ((num % 5) == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			if (num != 100)
+			printf("FizzBuzz");
+		}
+			else if (i == 1)
 			{
-				printf("Buzz ");
+				printf("%d", i);
 			}
-			else
-			{
-				printf("Buzz");
-			}
-		}
-		else
-		{
-			printf("%d ", num);
-		}
+				else
+				{
+					printf("%d", i);
+				}
 	}
 	printf("\n");
 
