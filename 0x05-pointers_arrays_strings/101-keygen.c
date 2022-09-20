61f82e44ec771c2dd5518e13cd196485d84aa73d
Gift111 <giftchukwudi99@gmail.com>
2022-09-19 20:45:20 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main - entry
+ *
+ * Return: password
+ */
+
+int main(void)
+{
+	char c;
+	int p;
+
+	srand(time(0));
+	while (x <= 2645)
+	{
+		c = rand() % 128;
+		p += c;
+	}
+	putchar(2772 - x);
+
+	return (0);
+}
