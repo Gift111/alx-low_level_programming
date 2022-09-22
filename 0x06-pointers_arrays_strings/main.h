81bc85b4f0543ab803823e99490a115e1b1cb41f
Gift111 <giftchukwudi99@gmail.com>
2022-09-21 18:09:34 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-/**
- * Description: Header file with prototypes
- *
- */
-
-int_putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -15,9 +6,9 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *rot13(char *);
+char *rot13(char *s);
 void print_number(int n);
+int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
-#endif /*MAIN_H*/
+void print_line(char *c, int s, int l);
