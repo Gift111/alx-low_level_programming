fba205b0d475a25e60578a490939de8e5d46791f
Gift111 <giftchukwudi99@gmail.com>
2022-10-03 16:33:32 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+}
