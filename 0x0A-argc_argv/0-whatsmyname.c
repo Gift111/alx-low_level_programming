bca8ad30e36013456321ed0ca42a3e39ffb765a0
Gift111 <giftchukwudi99@gmail.com>
2022-10-03 16:24:16 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,10 @@
  * @argc: Number of arguments
  * @argv: Array of pointers
  *
- * Return: 0
+ * Return: Always 0
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-printf("s\n", argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
