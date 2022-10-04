d1724c160b7a3f77a6a00dec25195ef83efc7893
Gift111 <giftchukwudi99@gmail.com>
2022-10-03 18:22:02 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, prod;
-if (argc != 3)
+int c, d;
+if (argc == 3)
 {
+c = atoi(argv[1]);
+d = atoi(argv[2]);
+printf("%d\n", c * d);
+return (0);
+}
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-prod = num 1 * num 2;
-
-printf("%d\n", prod);
-return (0);
