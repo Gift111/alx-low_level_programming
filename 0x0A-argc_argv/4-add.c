e76bf6c4e29ba87fe2b1f0a58d3a2aba6e30621d
Gift111 <giftchukwudi99@gmail.com>
2022-10-03 19:14:19 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Prints addition of positive numbers
  * @argc: number of arguments
- * @argv: array of pointers to arguments
+ * @argv: argument vector of pointers to strings
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-int num, digit, sum = 0;
-for (num = 1; num < argc; num++)
-{
-for (digit = 0; argv[num][digit]; digit++)
-{
-if (argv[num][digit] < '0' || argv[num][digit] > '9')
-{
-printf("Error\n");
-return (1);
-}
-}
-sum += atoi(argv[num]);
-}
-printf("%d\n", sun);
-return (0);
+	int a = 0, c, d;
+	for (c = 1; c < argc; c++)
+
+	{
+		for (d = 0; argv[c][d]; d++)
+		{
+			if (isdigit(argv[c][d]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (c = 1; c < argc; c++)
+	{
+		a += atoi(argv[c]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
