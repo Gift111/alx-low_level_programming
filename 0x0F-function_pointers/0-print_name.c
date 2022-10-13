cb1f784f94f600136e88ed4278766f95a66baebd
Gift111 <giftchukwudi99@gmail.com>
2022-10-13 15:51:02 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,7 @@
  * @name: name
  * @f: pointer to the printing function
  */
-
-void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name && f)
+		f(name);
 }
