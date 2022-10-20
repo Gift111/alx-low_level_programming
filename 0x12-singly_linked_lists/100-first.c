065c253d6418aa65aadbff21451676b77be6cbd8
Gift111 <giftchukwudi99@gmail.com>
2022-10-20 07:58:15 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,8 +7,8 @@
  * Return: none
  */
 
-void race(void)
+void __attribute__ ((constructor)) race()
 {
-	printf("You're beat! and yet, you must allow,"
-		"\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
