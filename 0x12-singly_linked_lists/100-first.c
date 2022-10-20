51a54daa074883129ab42fdafac9c2c87ee92437
Gift111 <giftchukwudi99@gmail.com>
2022-10-20 07:48:43 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * race - function that prints You're beat! and yet,
+ * you must allow,\nI bore my house upon my back!\n
+ * before the main function is executed.
+ * Return: none
+ */
+
+void race(void)
+{
+	printf("You're beat! and yet, you must allow,"
+		"\n I bore my house upon my back!\n");
+}
