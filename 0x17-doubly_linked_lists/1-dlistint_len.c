63990e452a9da4dc82f98094790c4d6d17018016
Gift111 <giftchukwudi99@gmail.com>
2022-12-09 17:37:27 -0800

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: head of linked list
+ *
+ * Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
